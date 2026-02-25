1428fe5ec3b84b9bcb88a97e36c1c83e5d989ca2
Cinjo Jose <cinjojose05@gmail.com>
2026-02-25 22:51:03 +0300

    First commit for Sample2

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => {   
+    res.send('Hello Cinjo! This is a sample Node.js application running on Azure App Service.');
+});
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
